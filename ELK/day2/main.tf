eaf6b88cd706ae6b3967953ce6fd2008d8a12320
Siarhei_Hryshchanka <zedex@tut.by>
2020-09-11 10:31:41 +0000

    elk_day2

--- ELK/day2/main.tf
+++ ELK/day2/main.tf
@@ -6,29 +6,28 @@ provider "google" {
   zone          = "us-central1-c"
 }
 
+
 //create elk instance 
 resource "google_compute_instance" "vm_elk" {
   name          = "elk-server"
   machine_type  = "n1-standard-1"
   tags          = ["http-server", "https-server", "elk"]
-
   boot_disk {
     initialize_params {
       image     = "centos-cloud/centos-7"
     }
   }
-
   metadata = {
     ssh-keys    = "${var.user_name}:${file(var.enter_path_to_public_key)}"
   }
   metadata_startup_script = file("elk_script.sh")
-
   network_interface {
     network     = "default"
     access_config {
     }
   }
 }
+
 
 locals {
   server_IP = google_compute_instance.vm_elk.network_interface.0.network_ip
@@ -39,7 +38,6 @@ resource "google_compute_instance" "vm_tomcat" {
   name          = "tomcat-server"
   machine_type  = "n1-standard-1"
   tags          = ["http-server", "https-server", "tomcat"]
-
   boot_disk {
     initialize_params {
       image     = "centos-cloud/centos-7"
@@ -47,30 +45,28 @@ resource "google_compute_instance" "vm_tomcat" {
   }
   //upload application for tomcat
   provisioner "file" {
-    source = "clusterjsp.war"
+    source      = "clusterjsp.war"
     destination = "/tmp/clusterjsp.war"
     connection {
       host = google_compute_instance.vm_tomcat.network_interface.0.access_config.0.nat_ip
       type = "ssh"
       user = "centos"
-      private_key = file(var.enter_path_to_private_key) 
       agent = "false"
+      private_key = file(var.enter_path_to_private_key)
     }
   }
-
-  metadata = {
+  metadata      = {
     ssh-keys    = "${var.user_name}:${file(var.enter_path_to_public_key)}"  
   }
-  
   metadata_startup_script = templatefile("tomcat_script.sh", { 
-    IP = "${local.server_IP}" })  //getting elk-server IP
-
+    IP          = "${local.server_IP}" })  //getting elk-server IP
   network_interface {
     network     = "default"
     access_config {
     }
   }
 }
+
 
 // create firewall rules for elk
 resource "google_compute_firewall" "kibana" {
@@ -94,7 +90,6 @@ resource "google_compute_firewall" "elastic" {
   source_ranges = ["0.0.0.0/0"]
   target_tags   = ["elk"] 
 }
-
 
 // create firewall rule for tomcat
 resource "google_compute_firewall" "tomcat" {
